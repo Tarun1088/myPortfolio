37f713d7a61e75ef473cd765bab2c24bddee0e8a
Tarun Rawal <116424495+Tarun1088@users.noreply.github.com>
2023-09-21 15:44:49 +0530

    Update scrollrestoration.jsx
    updated useEffect to useLayoutEffect for better rendering

--- src/components/scrollrestoration.jsx
+++ src/components/scrollrestoration.jsx
@@ -4,7 +4,7 @@ import { useLocation } from "react-router-dom";
 export default function ScrollToTop() {
   const { pathname } = useLocation();
 
-  useEffect(() => {
+ useLayoutEffect(() => {
     window.scrollTo(0, 0);
   }, [pathname]);
   return null;
